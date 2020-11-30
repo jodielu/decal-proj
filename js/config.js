0b69fec4a4ef24489b45ab42b3abaec77690429d
Jodie Lu <jodielu@Jodies-MacBook-Pro.local>
2020-11-30 14:56:01 -0800

    user pools again

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-west-2_gTuYN1RP8', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-west-2_gTuYN1RP8', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3b266o0ei7595g85kouaeq3m9a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
